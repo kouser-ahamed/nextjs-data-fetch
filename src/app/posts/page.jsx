65d30d68b8cb0807287ddf068eb97dabc293929a
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-20 16:21:31 +0600

    Remove commented-out code for fetching posts in PostsPage component

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,20 +1,24 @@
+// method 1
 // const getPosts = async () => {
 //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 //     return res.json();
 
 // };
 
-const getPosts = async () => {
-  try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// method 3
+// const getPosts = async () => {
+//   try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-    return res.json();
-  } catch (error) {
-    throw new Error("Fail to fetch posts");
-  }
-};
+//     return res.json();
+//   } catch (error) {
+//     throw new Error("Fail to fetch posts");
+//   }
+// };
 
 const PostsPage = async () => {
+  // method 2
+
   // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   // const posts = await res.json();
   // console.log(posts);
