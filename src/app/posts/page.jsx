e6538b216d8e2ef02989e9c28d7649f850b5e210
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-20 16:19:33 +0600

    Implement async data fetching for posts and update PostsPage component

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,10 +1,32 @@
+// const getPosts = async () => {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     return res.json();
 
-const PostsPage = () => {
-    return (
-        <div>
-            <h2>Post are coming soon!</h2>
-        </div>
-    );
+// };
+
+const getPosts = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    return res.json();
+  } catch (error) {
+    throw new Error("Fail to fetch posts");
+  }
+};
+
+const PostsPage = async () => {
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const posts = await res.json();
+  // console.log(posts);
+
+  const posts = await getPosts();
+  console.log(posts);
+
+  return (
+    <div>
+      <h2>Post are coming soon!: {posts.length}</h2>
+    </div>
+  );
 };
 
 export default PostsPage;
